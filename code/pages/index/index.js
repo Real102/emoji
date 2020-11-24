25c81c847e4f4366821a4145b3f6264bf503eba1
wolfBerry <906368017@qq.com>
2020-11-24 11:56:11 +0800

    fix：增加最近使用功能

--- code/pages/index/index.js
+++ code/pages/index/index.js
@@ -6,15 +6,54 @@ import {
 const app = getApp()
 Page({
   data: {
-    emojiList: emojiList
+    emojiList: emojiList,
+    recentlyList: [] // 近期使用emoji的列表
   },
   onLoad: function () {
-
-  },
-  copyText: function(event) {
-    let text = event.target.dataset.text
-    wx.setClipboardData({
-      data: text
+    wx.getStorage({
+      key: 'recentlyList',
+      success: res => {
+        this.setData({
+          recentlyList: res.data
+        })
+      },
+      fail: err => {
+        this.setData({
+          recentlyList: []
+        })
+      }
     })
+  },
+  copyText: function (event) {
+    let {
+      text,
+      key
+    } = event.target.dataset
+    if (text && key) {
+      wx.setClipboardData({
+        data: text
+      })
+      // 只能通过data选项来访问数据？
+      let recentlyList = this.data.recentlyList
+      // 查询是否已存在
+      let index = recentlyList.findIndex(item => item.key === key)
+      if (index > -1) {
+        // 如果已存在，那么直接删除，然后在头部重新插入
+        recentlyList.splice(index, 1)
+      } else if (recentlyList.length > 6) {
+        recentlyList.pop()
+      }
+      recentlyList.unshift({
+        key,
+        text
+      })
+      this.setData({
+        recentlyList
+      })
+      wx.setStorage({
+        data: recentlyList,
+        key: 'recentlyList',
+      })
+    }
   }
 })
